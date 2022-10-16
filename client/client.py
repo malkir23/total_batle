5cae36240ae0cfd3bcd9822a61580dff94f6544b
malkir23 <mandragorand@gmail.com>
2022-10-16 12:50:26 +0300

    change port

--- client/client.py
+++ client/client.py
@@ -2,8 +2,8 @@ import socket
 
 
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
+    host = "130.61.24.153"  # as both code is running on same pc
+    port = 10000  # socket server port number
 
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
